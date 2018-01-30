105c705042d85a555386992f16756f9384a3a014
gqueimado <35774918+gqueimado@users.noreply.github.com>
2018-01-30 15:09:36 +0000

    New Tc

---
+++ src/test/java/Test_Example7.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test_Example7 {
+    @Test
+
+    public void main() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Goncalo\\Desktop\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.pt/");
+        driver.findElement(By.xpath("//body[@id='gsr']")).isDisplayed();
+        driver.findElement(By.xpath("//input[@id='lst-ib']")).sendKeys("alten uk");
+        String ex = driver.findElement(By.xpath("//div[@id='footer']")).getText();
+        System.out.print(ex);
+        driver.findElement(By.xpath("//input[@id='lst-ib']")).clear();
+        driver.findElement(By.xpath("//input[@id='lst-ib']")).click();
+        driver.close();
+    }
+}
